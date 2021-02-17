3f75c4a4c0106ce60879dc3559d1ae7797cb5e8b
Nikolay Tereschenko <nikoteressi@gmail.com>
2021-02-17 21:39:00 +0300

    first commit

--- src/tax/Tax.java
+++ src/tax/Tax.java
@@ -6,34 +6,36 @@ public class Tax {
     private static final int TAX_30 = 30;
 
     public static void main(String[] args) {
-        double[] prices = new double[10];
-        fillArray(prices, 100, 20000);
-        getTax(prices);
+        getTax(args);
     }
 
-    private static void fillArray(double[] prices, double a, double b) {
-        for (int i = 0; i < prices.length; i++) {
-            prices[i] = a + Math.random() * b;
+    private static void getTax(String[] args) {
+        for (String price : args) {
+            if (isNumber(price)) {
+                double priceNumber = Double.parseDouble(price);
+                if (priceNumber <= 0 || priceNumber > 10000) {
+                    System.out.printf("Для суммы %1f налог не предусмотрен! \n", priceNumber);
+                } else if (priceNumber < 100) {
+                    printPriceWithTax(priceNumber, TAX_10);
+                } else if (priceNumber < 1000) {
+                    printPriceWithTax(priceNumber, TAX_20);
+                } else {
+                    printPriceWithTax(priceNumber, TAX_30);
+                }
+
+            } else {
+                System.out.printf("%s не является числовым значением\n", price);
+            }
         }
     }
-    private static void getTax(double[] prices){
-        double priceWithTax = 0;
-        for (double price : prices) {
-            if (price > 0 && price < 100) {
-                priceWithTax = (price * TAX_10) / 100;
-            }
-            if (price > 100 && price < 1000) {
-                priceWithTax = (price * TAX_20) / 100;
-            }
-            if (price > 1000 && price < 10000) {
-                priceWithTax = (price * TAX_30) / 100;
-            }
-            if (price<0 || price>10000){
-                    System.out.printf("Для суммы %1f налог не предусмотрен! \n", price);
-                    continue;
-            }
-            System.out.printf("Налог для суммы %1f = %1f \n", price, priceWithTax);
-        }
+
+    private static void printPriceWithTax(double priceNumber, int tax) {
+        double priceWithTax = (priceNumber * tax) / 100;
+        System.out.printf("Налог для суммы %1f = %1f \n", priceNumber, priceWithTax);
+    }
+
+    private static boolean isNumber(String price) {
+        return price.matches("-?\\d+(\\.\\d*)?");
     }
 }
 
